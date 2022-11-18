2be321ab1d4bfa90d54ef580f3e82058dd5f9d0c
fermer123 <fermer_525@mail.ru>
2022-11-18 23:37:14 +0500

    swiper 8.4 instead 6

--- src/components/slides/Slide.tsx
+++ src/components/slides/Slide.tsx
@@ -2,27 +2,31 @@ import React from 'react';
 import { iventItem, useCustomContext } from '../context/Context';
 import style from './Slide.module.scss';
 import 'swiper/swiper.scss';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { Navigation } from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react/swiper-react.js';
+import 'swiper/modules/navigation/navigation.scss';
+import 'swiper/modules/pagination/pagination.scss'; // Pagination module
+import { Navigation, Pagination } from 'swiper';
 
 const Slide: React.FC = () => {
   const { data, year } = useCustomContext();
 
   return (
-    <Swiper
-      className={style.mySwiper}
-      modules={[Navigation]}
-      navigation
-      spaceBetween={80}
-      slidesPerView={5}
-    >
-      {data[year - 1].events.map((e: iventItem) => (
-        <SwiperSlide className={style.swiperslide} key={e.year}>
-          <p className={style.year}>{e.year}</p>
-          <p className={style.content}>{e.content}</p>
-        </SwiperSlide>
-      ))}
-    </Swiper>
+    <div>
+      <Swiper
+        modules={[Navigation, Pagination]}
+        navigation
+        spaceBetween={80}
+        slidesPerView={3}
+        className={style.mySwiper}
+      >
+        {data[year - 1].events.map((e: iventItem) => (
+          <SwiperSlide className={style.swiperslide} key={e.year}>
+            <p className={style.year}>{e.year}</p>
+            <p className={style.content}>{e.content}</p>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
   );
 };
 
